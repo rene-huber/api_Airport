42100cc07e4af1e0ed4092322b51dee85e413a06
Rene Huber <huberskull@gmail.com>
2022-12-14 03:24:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import "./App.css";
+import Axios from "./main-components/Axios";
+import Fetch from "./main-components/Fetch";
+
+
+
+function App() {
+  return (
+    <div className="App">
+ {/* <Fetch /> */}
+      <Axios />
+    
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main-components/Axios.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Axios() {
+  const options = {
+    method: 'GET',
+    url: 'https://gibraltar-airport-feed.p.rapidapi.com/next-flight',
+    headers: {
+      'X-RapidAPI-Key': 'b60a40ebbamsh883889bbffc9653p11c47fjsnc009c264e199',
+      'X-RapidAPI-Host': 'gibraltar-airport-feed.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data.collection.items[0].data[0].value);
+  
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+export default Axios;
